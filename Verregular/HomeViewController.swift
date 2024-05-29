3aa989d9b4d1160829be99c3f951d555d2e2684a
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-05-29 18:47:10 +0500

    Create Verregular app and add first commit

--- Verregular/HomeViewController.swift
+++ Verregular/HomeViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    // MARK: - GUI Variables
+    
+    // MARK: - Properties
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    // MARK: - Methods
 }
 
