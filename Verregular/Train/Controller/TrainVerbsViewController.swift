f6cff56a04bece0162b126288b39a2ec465530ce
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-05-31 12:42:58 +0500

    Add firstVC and localization

---
+++ Verregular/Train/Controller/TrainVerbsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TrainVerbsViewController.swift
+//  Verregular
+//
+//  Created by Даниил Сивожелезов on 30.05.2024.
+//
+
+import UIKit
+
+final class TrainVerbsViewController: UIViewController {
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
