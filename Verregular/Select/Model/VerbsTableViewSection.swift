542728ab6c6ce9ad303c9a5ffc73e5da7af7064a
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-06-18 13:55:13 +0500

    Add sections for the verbs

---
+++ Verregular/Select/Model/VerbsTableViewSection.swift
@@ -0,0 +1,13 @@
+//
+//  VerbsTableViewSection.swift
+//  Verregular
+//
+//  Created by Даниил Сивожелезов on 18.06.2024.
+//
+
+import Foundation
+
+struct VerbsTableViewSection {
+    var title: String
+    var verbs: [Verb]
+}
